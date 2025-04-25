060a3175806bad8d185c7a8be8997136e5b94466
Pavel Drozdov <73004817+rei-cher@users.noreply.github.com>
2025-04-25 16:49:09 -0400

    Update cmm-cookie.js

--- func/cmm-cookie.js
+++ func/cmm-cookie.js
@@ -1,52 +1,26 @@
-function getCookie(tabUrl, callback) {
-    chrome.cookies.get(
-        { url: tabUrl, name: "cmm_production_session" },
-        (cookie) => {
-            if (chrome.runtime.lastError) {
-                console.error("Cookie API error:", chrome.runtime.lastError);
-                return callback(null);
-            }
-            callback(cookie ? cookie.value : null);
-        }
-    );
-}
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (!changeInfo.url) return;
+  // only when the page is fully loaded…
+  if (changeInfo.status !== 'complete') return;
 
-    const url = changeInfo.url;
-    if (!url.includes("/v2/requests")) return;
-  
-    const pa_id = url.split("/").pop();
+  const url = tab.url;
+  if (!url.includes('/v2/requests/')) return;
 
-    getCookie(url, (token) => {
-        if (!token) {
-            console.error("No session token found for", url);
-            return;
-        }
+  const pa_id = url.split('/').pop();
 
-        console.log(
-            `Parameters for sending in the message:\n  Token: ${token}\n  PA ID: ${pa_id}`
-        );
+  getCookie(url, token => {
+    if (!token) return console.error('No session token for', url);
 
-        // TODO: call the function when the form is submitted - status code 200 
-        if (typeof getPatientInfo === 'function'){
-            getPatientInfo(pa_id)
-                .then(data => {
-                    console.log(`PA data: ${data}`);
-                    console.log(`PT drug: ${data.drug.replaceAll(" ", "-")}`);
-                    console.log(`PT first name: ${data.patient_fname}`);
-                    console.log(`PT last name: ${data.patient_lname}`);
-                    console.log(`PT dob: ${data.sections[1].rows[1].questions[0].answer_text}`);
+    if (typeof getPatientInfo !== 'function') {
+      return console.error('getPatientInfo() not defined');
+    }
 
-                    downloadPA(pa_id, data.patient_fname, data.patient_lname, data.drug.replaceAll(" ", "-"));
-                })
-                .catch(error => {
-                    console.error(`Error fetching patient info: ${error}`)
-                });
-        }
-        else {
-            console.error("getPatientInfo() not defined")
-        }
-    });
+    getPatientInfo(pa_id)
+      .then(data => {
+        downloadPA(pa_id,
+                   data.patient_fname,
+                   data.patient_lname,
+                   data.drug.replaceAll(' ', '-'));
+      })
+      .catch(err => console.error('Error fetching PA info:', err));
+  });
 });
